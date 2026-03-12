bc7730fd1bb159ed7d221b1ca4d50566c87eaaf3
Kendriick <kenjclee03@gmail.com>
2026-03-13 00:19:53 +1100

    ADD app tsx
    - initial page setup to grab conversation id from project, temporary divs for content layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import { projects } from "./lib/mockData";
+
 function App() {
-  return <div>CCCT</div>
+  const [selectedConversationId, setSelectedConversationId] = useState<string | null>(null);
+
+  const selectedConversation = projects
+  .flatMap(project => project.conversations)
+  .find(convo => convo.id === selectedConversationId);
+
+  return(
+    <>
+      <div className="flex h-full">
+        <div className="w-[240px]">
+          <p>Sidebar</p>
+        </div>
+        <div className="flex-1">
+          <p>Convo view</p>
+        </div>
+        <div className="w-[320px]">
+          <p>Output panel</p>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App
+
+
+export default App;
