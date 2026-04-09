f59f7406140ba7230db491dbc8efc07af1f61a87
Kendriick <kenjclee03@gmail.com>
2026-04-10 00:19:48 +1000

    UPDATE topbar
    - added props for settings button

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,11 +1,15 @@
 import { Settings } from "lucide-react";
 
-export default function TopBar() {
+type TopBarProps = {
+  onSettingsClick: () => void
+}
+
+export default function TopBar({ onSettingsClick } : TopBarProps) {
   return(
     <div className="flex items-center justify-between px-4 py-3 border-b border-surface-border">
       <h1 className="text-sm font-semibold text-text-primary">CCCT</h1>
 
-      <button className="text-text-muted">
+      <button className="text-text-muted" onClick={onSettingsClick}>
         <Settings size={14}/>
       </button>
     </div>
